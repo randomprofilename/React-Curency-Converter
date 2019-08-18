bf38ec90c3c555e6b5888aa7d19278d81a1f8fd3
randomprofilename <zeum73@gmail.com>
2019-08-19 00:38:03 +0700

    background theme. countries on table

--- src/components/Currencies/CurrenciesListTable.js
+++ src/components/Currencies/CurrenciesListTable.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { Table } from "semantic-ui-react";
+import { Table, Image } from "semantic-ui-react";
 
 const CurrenciesList = props => {
-  const { rates = {} } = props;
+  const { rates = {}, countries = {} } = props;
 
   return <Table celled>
     <Table.Header>
-      <Table.Row>
+      <Table.Row style={{ textAlign: "center" }} >
         <Table.HeaderCell>Currency</Table.HeaderCell>
+        <Table.HeaderCell>Country Name</Table.HeaderCell>
+        <Table.HeaderCell>Flag</Table.HeaderCell>
         <Table.HeaderCell>Rate</Table.HeaderCell>
       </Table.Row>
     </Table.Header>
     <Table.Body>
       {Object.keys(rates).map((currency, index) => 
-        <Table.Row key={index}> 
+        <Table.Row key={index}>
           <Table.Cell>{currency}</Table.Cell> 
+          <Table.Cell>{countries[currency].name}</Table.Cell> 
+          <Table.Cell> <Image centered size="mini" src={countries[currency].flag} /></Table.Cell>
           <Table.Cell>{rates[currency]}</Table.Cell> 
         </Table.Row>)          
       }
